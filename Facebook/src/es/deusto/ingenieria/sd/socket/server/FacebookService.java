705fef55373fb3fedf170782502de8df3d5d411a
IkerPerezBralo <iker.perez.bralo@opendeusto.es>
2023-11-25 15:12:43 +0100

    Parte servidor Facebook insertada

---
+++ Facebook/src/es/deusto/ingenieria/sd/socket/server/FacebookService.java
@@ -0,0 +1,52 @@
+package es.deusto.ingenieria.sd.socket.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * This class process the request of each client as a separated Thread.
+ */
+public class FacebookService extends Thread {
+	private DataInputStream in;
+	private DataOutputStream out;
+	private Socket tcpSocket;
+
+	public FacebookService(Socket socket) {
+		try {
+			this.tcpSocket = socket;
+			this.in = new DataInputStream(socket.getInputStream());
+			this.out = new DataOutputStream(socket.getOutputStream());
+			this.start();
+		} catch (IOException e) {
+			System.err.println("# FacebookService - TCPConnection IO error:" + e.getMessage());
+		}
+	}
+
+	public void run() {
+		// Facebook server
+		try {
+			// Read request from the client
+			String data = this.in.readUTF();
+			System.out.println("   - FacebookService - Received data from '" + tcpSocket.getInetAddress().getHostAddress()
+					+ ":" + tcpSocket.getPort() + "' -> '" + data + "'");
+
+			// Send response to the client
+			this.out.writeUTF(data.toUpperCase());
+			System.out.println("   - FacebookService - Sent data to '" + tcpSocket.getInetAddress().getHostAddress() + ":"
+					+ tcpSocket.getPort() + "' -> '" + data.toUpperCase() + "'");
+		} catch (EOFException e) {
+			System.err.println("   # FacebookService - TCPConnection EOF error" + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("   # FacebookService - TCPConnection IO error:" + e.getMessage());
+		} finally {
+			try {
+				tcpSocket.close();
+			} catch (IOException e) {
+				System.err.println("   # FacebookService - TCPConnection IO error:" + e.getMessage());
+			}
+		}
+	}
+}
